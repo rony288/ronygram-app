695a34349243b5804afd9dac004033b8a9a546de
rony288 <146983349+rony288@users.noreply.github.com>
2025-05-07 22:53:22 -0400

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -5,12 +5,12 @@ const Users = () => {
     return (
         <div className="image-icons-container">
             <div className="image-icon"><img src={require('../images/Kimurgor.jpeg')} alt="Icon 1"/></div>
-            <div className="image-icon"><img src={require('../images/2.jpg')} alt="Icon 2"/></div>
-            <div className="image-icon"><img src={require('../images/3.jpg')} alt="Icon 3"/></div>
-            <div className="image-icon"><img src={require('../images/4.jpg')} alt="Icon 4"/></div>
+            <div className="image-icon"><img src={require('../images/PB.jpg')} alt="Icon 2"/></div>
+            <div className="image-icon"><img src={require('../images/VB.jpg')} alt="Icon 3"/></div>
+            <div className="image-icon"><img src={require('../images/Radical Roots.jpg')} alt="Icon 4"/></div>
             <div className="image-icon"><img src={require('../images/5.jpg')} alt="Icon 5"/></div>
-            <div className="image-icon"><img src={require('../images/6.jpg')} alt="Icon 6"/></div>
-            <div className="image-icon"><img src={require('../images/7.jpg')} alt="Icon 7"/></div>
+            <div className="image-icon"><img src={require('../images/Moo.jpg')} alt="Icon 6"/></div>
+            <div className="image-icon"><img src={require('../images/Blue.jpg')} alt="Icon 7"/></div>
             <div className="image-icon"><img src={require('../images/8.jpg')} alt="Icon 8"/></div>
         </div>
     );
