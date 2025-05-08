aa99d18f1cce19140fafb0bb7f34cf3bc1dbc322
rony288 <146983349+rony288@users.noreply.github.com>
2025-05-07 23:01:11 -0400

    Update Users.js

--- src/components/Users.js
+++ src/components/Users.js
@@ -8,10 +8,10 @@ const Users = () => {
             <div className="image-icon"><img src={require('../images/PB.jpg')} alt="Icon 2"/></div>
             <div className="image-icon"><img src={require('../images/VB.jpg')} alt="Icon 3"/></div>
             <div className="image-icon"><img src={require('../images/Radical Roots.jpg')} alt="Icon 4"/></div>
-            <div className="image-icon"><img src={require('../images/5.jpg')} alt="Icon 5"/></div>
+            <div className="image-icon"><img src={require('../images/Door.jpeg')} alt="Icon 5"/></div>
             <div className="image-icon"><img src={require('../images/Moo.jpg')} alt="Icon 6"/></div>
             <div className="image-icon"><img src={require('../images/Blue.jpg')} alt="Icon 7"/></div>
-            <div className="image-icon"><img src={require('../images/8.jpg')} alt="Icon 8"/></div>
+            <div className="image-icon"><img src={require('../images/ML.jpeg')} alt="Icon 8"/></div>
         </div>
     );
 };
