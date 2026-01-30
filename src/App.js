c981d14eb6e1defe9c6fe52172cd86e702038463
Ronald Kipkemboi <146983349+rony288@users.noreply.github.com>
2026-01-30 18:37:08 -0500

    Refactor App.js for import spacing and className
    Fixed spacing in import statements and added className to the main div.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login';
 import Home from './components/Home';
 import Profile from './components/Profile';
 import Settings from './components/Settings';
 import Explore from './components/Explore';
-import'./App.css';
+import './App.css'; // Fixed spacing here
 
 function App() {
   return (
     <Router>
-      <div>
+      {/* Added className="App" to apply the fancy background */}
+      <div className="App">
         <Routes>
-          <Route path="/" element={<Login/>}/>
-          <Route path="/Home"element={<Home/>}/>
-          <Route path="/Profile"element={<Profile/>}/>
-          <Route path="/Settings"element={<Settings/>}/>
-          <Route path="/Explore"element={<Explore/>}/>
+          <Route path="/" element={<Login />} />
+          <Route path="/Home" element={<Home />} />
+          <Route path="/Profile" element={<Profile />} />
+          <Route path="/Settings" element={<Settings />} />
+          <Route path="/Explore" element={<Explore />} />
         </Routes>
       </div>
     </Router>
